8d8de4c618a061350bcdb1c95adee3a0d8190156
nmisustin <nmisustin@gmail.com>
2021-02-27 22:03:49 -0800

    added fight function and name variable

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+function fight(){
+    window.alert("The fight has begun!");
+}
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName)
+console.log("this log is a string good for leaving a message");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName)
+//fight();
