ec98319a63d11a482d12fb452c62515ff76dd0bb
nmisustin <nmisustin@gmail.com>
2021-03-02 21:11:33 -0800

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,14 +22,15 @@ var i =0;
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
-          console.log("playerMoney", playerMoney)
+          playerMoney = Math.max(0, playerMoney-10);
+          console.log("playerMoney", playerMoney);
           break;
         }
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      var playerDamage = randomNumber(playerAttack - 3, playerAttack)
+      enemyHealth = Math.max(0, enemyHealth - playerDamage);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -48,7 +49,8 @@ var i =0;
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack-3, enemyAttack)
+      playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -73,7 +75,7 @@ var startGame = function (){
     if (playerHealth > 0){
       window.alert ("Welcome to Robot Gladiators! Round " + (i+1));
       var pickedEnemyName = enemyName [i];
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
       fight(pickedEnemyName);
       if (playerHealth > 0 && i< enemyName.length - 1){
         var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
@@ -149,6 +151,11 @@ var shop = function(){
       shop();
       break;
   }
+}
+//function generates randome number
+var randomNumber = function(min, max){
+  var value = Math.floor(Math.random()*(max -min + 1)+min);
+  return value;
 }
 //start the game when the page loads
 startGame ();
