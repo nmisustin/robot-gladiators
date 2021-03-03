939667941eb06889c863f8c8525d0cad41c3a4e5
nmisustin <nmisustin@gmail.com>
2021-03-02 19:07:59 -0800

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -75,6 +75,13 @@ var startGame = function (){
       var pickedEnemyName = enemyName [i];
       enemyHealth = 50;
       fight(pickedEnemyName);
+      if (playerHealth > 0 && i< enemyName.length - 1){
+        var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+        //if yes, take them to the store function
+        if(storeConfirm){
+          shop();
+        }
+      }
     }
     else {
       window.alert ("You have lost your robot in battle! Game over!");
@@ -100,6 +107,47 @@ var endGame = function() {
   }
   else {
     window.alert ("Thank you for playing Robot Gladiators! Come back soon!")
+  }
+}
+var shop = function(){
+  console.log("entered the shop");
+  var shopOptionPrompt = window.prompt(
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', or 'UPGRADE', or 'LEAVE' to make a choice."
+  )
+  switch(shopOptionPrompt) {
+    case "REFILL":
+    case "refill":
+      if (playerMoney >= 7){
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+        //increase health and decrease money
+        playerHealth = playerHealth + 20;
+        playerMoney = playerMoney - 7;
+      }
+      else{
+        window.alert("You don't have enought money!");
+      }
+      break;
+    case "UPGRADE":
+    case "upgrade":
+      if (playerMoney >= 7){
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+        //increase attack and decrease money
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney - 7;
+      }
+      else{
+        window.alert("You don't have enought money!");
+      }
+      break;
+    case "LEAVE":
+    case "leave":
+      window.alert("Leaving the store,");
+      //end function
+      break;
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+      shop();
+      break;
   }
 }
 //start the game when the page loads
